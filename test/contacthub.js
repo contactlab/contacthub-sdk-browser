c0ab2010441c118dc539457e871561e09e795aa1
Francesco Negri <francesconegri@gmail.com>
2016-08-04 13:11:42 +0200

    add tests for 'config'

--- test/contacthub.js
+++ test/contacthub.js
@@ -4,20 +4,23 @@ import cookies from 'js-cookie';
 
 const apiUrl = 'https://api.contactlab.it/hub/v1/';
 const cookieName = '_ch';
+const varName = 'ch';
 
 const getCookie = () => cookies.getJSON(cookieName) || {};
 
 /* global describe, it, beforeEach */
 
 describe('contacthub.js', () => {
-  it('creates the window.ch function', () => {
-    expect(window.ch).to.be.a('function');
+  const _ch = window[varName];
+
+  it('creates the _ch function', () => {
+    expect(_ch).to.be.a('function');
   });
 
   describe('Config API', () => {
     beforeEach(() => {
-      window.ch('config', {
-        workspaceId: 'w_id',
+      _ch('config', {
+        workspaceId: 'workspace_id',
         nodeId: 'node_id',
         token: 'ABC123'
       });
@@ -29,6 +32,42 @@ describe('contacthub.js', () => {
 
     it('generates a UUIDv4 sessionId', () => {
       expect(getCookie().sid).to.match(/^[0-9A-F]{8}-[0-9A-F]{4}-[4][0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$/i);
+    });
+
+    it('does not regenerate sessionId if already present', () => {
+      const sid = getCookie().sid;
+      _ch('config');
+      expect(getCookie().sid).to.equal(sid);
+    });
+
+    it('stores all config data in the cookie', () => {
+      const c = getCookie();
+      expect(c.workspaceId).to.equal('workspace_id');
+      expect(c.nodeId).to.equal('node_id');
+      expect(c.token).to.equal('ABC123');
+    });
+
+    it('uses "WEB" if context not provided', () => {
+      expect(getCookie().context).to.equal('WEB');
+    });
+
+    it('allows to specify optional context', () => {
+      _ch('config', {
+        workspaceId: 'workspace_id',
+        nodeId: 'node_id',
+        token: 'ABC123',
+        context: 'foo'
+      });
+      expect(getCookie().context).to.equal('WEB');
+    });
+
+    it('allows to override a single required param', () => {
+      _ch('config', {
+        nodeId: 'another_node'
+      });
+      expect(getCookie().nodeId).to.equal('another_node');
+      expect(getCookie().workspaceId).to.equal('workspace_id');
+      expect(getCookie().token).to.equal('ABC123');
     });
   });
 
@@ -47,7 +86,7 @@ describe('contacthub.js', () => {
     });
 
     it('sends the event to the API', (done) => {
-      window.ch('event', 'viewed_page').then(() => {
+      _ch('event', 'viewed_page').then(() => {
         // console.log('WUT', res);
         done();
       }).catch((err) => {
