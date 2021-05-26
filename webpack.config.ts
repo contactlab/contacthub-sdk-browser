51e46756b03d1425cdd144ffa888ccf2a22657db
StefanoMagrassi <1291271+StefanoMagrassi@users.noreply.github.com>
2021-05-26 12:26:13 +0200

    fix build pipeline

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,10 +1,13 @@
+import TerserPlugin from 'terser-webpack-plugin';
 import {Configuration, RuleSetRule} from 'webpack';
+import * as webpack from 'webpack';
 import {merge} from 'webpack-merge';
 
 const commons: Configuration = {
   mode: 'production',
+  devtool: 'source-map',
   entry: {
-    sdk: './src/index.js'
+    sdk: './src/index.ts'
   },
   output: {
     library: 'ContactlabSDKBrowser',
@@ -13,7 +16,11 @@ const commons: Configuration = {
   optimization: {
     emitOnErrors: false,
     concatenateModules: true
-  }
+  },
+  resolve: {
+    extensions: ['.ts', '.js']
+  },
+  plugins: [new webpack.ProgressPlugin()]
 };
 
 const tsLoader = (configFile: string): RuleSetRule => ({
@@ -33,13 +40,31 @@ const tsLoader = (configFile: string): RuleSetRule => ({
 const es6 = merge(commons, {
   target: ['web', 'es6'],
 
-  module: {rules: [tsLoader('tsconfig.build.json')]}
+  module: {rules: [tsLoader('tsconfig.build.json')]},
+
+  optimization: {
+    minimizer: [
+      new TerserPlugin({
+        terserOptions: {ecma: 2015},
+        parallel: true
+      })
+    ]
+  }
 });
 
 const es5 = merge(commons, {
   target: ['web', 'es5'],
 
-  module: {rules: [tsLoader('tsconfig.build.legacy.json')]}
+  module: {rules: [tsLoader('tsconfig.build.legacy.json')]},
+
+  optimization: {
+    minimizer: [
+      new TerserPlugin({
+        terserOptions: {ecma: 5},
+        parallel: true
+      })
+    ]
+  }
 });
 
 export const latest = merge(es6, {optimization: {minimize: false}});
