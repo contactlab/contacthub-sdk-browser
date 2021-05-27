6c0be03ad6c5d5d136b5ed893716d7c84ae82d84
StefanoMagrassi <1291271+StefanoMagrassi@users.noreply.github.com>
2021-05-27 16:01:47 +0200

    use fetchMock and helpers in consents tests

--- test/integration/consents.test.ts
+++ test/integration/consents.test.ts
@@ -2,18 +2,75 @@
 
 import {expect} from 'chai';
 import cookies from 'js-cookie';
-import sinon from 'sinon';
-import {ConfigOptions} from '../../src/config';
 import {CustomerData} from '../../src/customer';
+import * as H from './_helpers';
 
-const apiUrl = 'https://api.contactlab.it/hub/v1';
-const cookieName = '_ch';
+describe('Consents', () => {
+  beforeEach(() => {
+    cookies.remove(H.CH);
 
-const config: ConfigOptions = {
-  workspaceId: 'workspace_id',
-  nodeId: 'node_id',
-  token: 'ABC123'
-};
+    H._ch('config', H.CONFIG);
+  });
+
+  afterEach(done => {
+    H._fetchMock.resetHistory();
+
+    H.whenDone(() => undefined, done);
+  });
+
+  it('can be set', done => {
+    H._fetchMock
+      .post(`${H.API}/workspaces/${H.WSID}/customers`, {id: CID})
+      .post(`${H.API}/workspaces/${H.WSID}/customers/${CID}/sessions`, 200);
+
+    H._ch('customer', CUSTOMER);
+
+    H.whenDone(() => {
+      const [create] = H._fetchMock.calls();
+      const [, opts] = create;
+      const body = opts?.body as unknown as string;
+
+      expect(JSON.parse(body).consents).to.eql(CUSTOMER.consents);
+    }, done);
+  });
+
+  it('can be updated', done => {
+    H._fetchMock.mock(
+      `begin:${H.API}/workspaces/${H.WSID}/customers/${CID}`,
+      200
+    );
+
+    cookies.set(H.CH, {...cookies.getJSON(H.CH), customerId: CID});
+
+    H._ch('customer', CUSTOMER);
+
+    H.whenDone(() => {
+      H._ch('customer', {
+        ...CUSTOMER,
+        consents: {
+          softSpam: {email: {objection: true}}
+        }
+      });
+
+      H.whenDone(() => {
+        const body = H._fetchMock.lastOptions()?.body as unknown as string;
+        const method = H._fetchMock.lastOptions()?.method;
+        const url = H._fetchMock.lastUrl();
+
+        expect(method).to.equal('PATCH');
+        expect(url).to.equal(
+          `${H.API}/workspaces/${H.CONFIG.workspaceId}/customers/${CID}`
+        );
+        expect(JSON.parse(body).consents).to.eql({
+          softSpam: {email: {objection: true}}
+        });
+      }, done);
+    });
+  });
+});
+
+// --- Helpers
+const CID = 'abcd';
 
 const CUSTOMER: CustomerData = {
   externalId: 'foo.bar',
@@ -40,83 +97,3 @@ const CUSTOMER: CustomerData = {
     }
   }
 };
-
-const _ch = window.ch;
-
-const getCookie = () => cookies.getJSON(cookieName) || {};
-
-const setConfig = () => {
-  _ch('config', config);
-};
-
-let requests: sinon.SinonFakeXMLHttpRequest[] = [];
-let xhr: sinon.SinonFakeXMLHttpRequestStatic;
-
-// Mocked Ajax calls return immediately but need a short setTimeout to avoid
-// race conditions. 0 ms works fine on all browsers except IE 10 which requires
-// at least 2 ms.
-// TODO: find a more elegant way to mock Ajax calls
-const whenDone = (f: () => void): void => {
-  setTimeout(() => f(), 2);
-};
-
-describe('Consents', () => {
-  beforeEach(() => {
-    cookies.remove(cookieName);
-    requests = [];
-    xhr = sinon.useFakeXMLHttpRequest();
-    xhr.onCreate = r => {
-      requests.push(r);
-    };
-    setConfig();
-  });
-
-  afterEach(done => {
-    whenDone(() => done());
-  });
-
-  it('can be set', () => {
-    _ch('customer', CUSTOMER);
-
-    expect(requests.length).to.equal(1);
-    const req = requests[0];
-    expect(req.method).to.equal('POST');
-    expect(req.url).to.equal(
-      `${apiUrl}/workspaces/${config.workspaceId}/customers`
-    );
-    expect(JSON.parse(req.requestBody).consents).to.eql(CUSTOMER.consents);
-  });
-
-  it('can be updated', done => {
-    cookies.set(cookieName, {...getCookie(), customerId: 'my-cid'});
-
-    _ch('customer', CUSTOMER);
-
-    requests[0].respond(200, {}, '');
-
-    whenDone(() => {
-      _ch('customer', {
-        ...CUSTOMER,
-        consents: {
-          softSpam: {email: {objection: true}}
-        }
-      });
-
-      expect(requests.length).to.equal(2);
-
-      whenDone(() => {
-        expect(requests.length).to.equal(2);
-        const req = requests[1];
-        expect(req.method).to.equal('PATCH');
-        expect(req.url).to.equal(
-          `${apiUrl}/workspaces/${config.workspaceId}/customers/my-cid`
-        );
-        expect(JSON.parse(req.requestBody).consents).to.eql({
-          softSpam: {email: {objection: true}}
-        });
-
-        done();
-      });
-    });
-  });
-});
