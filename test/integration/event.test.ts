c1672528aa3fd98ed7c12b4e5494060ac5409312
StefanoMagrassi <1291271+StefanoMagrassi@users.noreply.github.com>
2021-05-28 10:46:24 +0200

    fix events tests

--- test/integration/event.test.ts
+++ test/integration/event.test.ts
@@ -1,107 +1,97 @@
-/* global describe, it, beforeEach, afterEach */
-
 import {expect} from 'chai';
 import cookies from 'js-cookie';
-import sinon from 'sinon';
 import * as H from './_helpers';
-
-const setConfig = (): void => H._ch('config', H.CONFIG);
-
-let requests: sinon.SinonFakeXMLHttpRequest[];
-const spy = sinon.stub(console, 'error').callsFake(() => undefined);
 
 describe('Event API', () => {
   beforeEach(() => {
     cookies.remove(H.CH);
+
+    H._fetchMock.post(`${H.API}/workspaces/${H.WSID}/events`, 200);
   });
 
   afterEach(() => {
-    spy.resetHistory();
+    H.spy.resetHistory();
 
     H._fetchMock.resetHistory();
   });
 
-  it('checks if required config is set', done => {
+  it('checks if required config is set', async () => {
     expect(() => {
       H._ch('event', {type: 'viewedPage'});
     }).not.to.throw();
 
-    H.whenDone(() => {
-      expect(
-        spy.calledWith(
-          '[DEBUG] @contactlab/sdk-browser',
-          'Missing "_ch" cookie'
-        )
-      ).to.equal(true);
+    await H.whenDone();
 
-      expect(H._fetchMock.called()).to.equal(false);
-    }, done);
+    expect(
+      H.spy.calledWith(
+        '[DEBUG] @contactlab/sdk-browser',
+        'Missing "_ch" cookie'
+      )
+    ).to.equal(true);
+
+    expect(H._fetchMock.called()).to.equal(false);
   });
 
-  it('sends the event to the API', done => {
-    H._fetchMock.post(`${H.API}/workspaces/${H.WSID}/events`, 200);
-
+  it('sends the event to the API', async () => {
     setConfig();
 
     H._ch('event', {type: 'viewedPage'});
 
-    H.whenDone(() => {
-      const headers = H._fetchMock.lastOptions()?.headers;
-      const body = JSON.parse(
-        H._fetchMock.lastOptions()?.body as unknown as string
-      );
+    await H.whenDone();
 
-      expect(headers?.Authorization).to.equal(`Bearer ${H.TOKEN}`);
-      expect(body.type).to.equal('viewedPage');
-      expect(body.bringBackProperties).to.eql({
-        type: 'SESSION_ID',
-        value: cookies.getJSON(H.CH).sid,
-        nodeId: H.NID
-      });
-    }, done);
+    const headers = H._fetchMock.lastOptions()?.headers;
+    const body = JSON.parse(
+      H._fetchMock.lastOptions()?.body as unknown as string
+    );
+
+    expect(headers?.Authorization).to.equal(`Bearer ${H.TOKEN}`);
+    expect(body.type).to.equal('viewedPage');
+    expect(body.bringBackProperties).to.eql({
+      type: 'SESSION_ID',
+      value: cookies.getJSON(H.CH).sid,
+      nodeId: H.NID
+    });
   });
 
-  it('sends customerId when available in cookie', done => {
-    H._fetchMock.post(`${H.API}/workspaces/${H.WSID}/events`, 200);
-
+  it('sends customerId when available in cookie', async () => {
     setConfig();
 
     cookies.set(H.CH, {...cookies.getJSON(H.CH), customerId: H.CID});
 
     H._ch('event', {type: 'viewedPage'});
 
-    H.whenDone(() => {
-      const body = H._fetchMock.lastOptions()?.body as unknown as string;
+    await H.whenDone();
 
-      expect(JSON.parse(body).customerId).to.eql(H.CID);
-    }, done);
+    const body = H._fetchMock.lastOptions()?.body as unknown as string;
+
+    expect(JSON.parse(body).customerId).to.eql(H.CID);
   });
 
-  it.only('omits bringBackProperties when customerId is available', done => {
-    H._fetchMock.post(`${H.API}/workspaces/${H.WSID}/events`, 200);
-
+  it('omits bringBackProperties when customerId is available', async () => {
     setConfig();
 
     cookies.set(H.CH, {...cookies.getJSON(H.CH), customerId: H.CID});
 
     H._ch('event', {type: 'viewedPage'});
 
-    H.whenDone(() => {
-      const body = H._fetchMock.lastOptions()?.body as unknown as string;
+    await H.whenDone();
 
-      expect(JSON.parse(body).bringBackProperties).to.equal(undefined);
-    }, done);
+    const body = H._fetchMock.lastOptions()?.body as unknown as string;
+
+    expect(JSON.parse(body).bringBackProperties).to.equal(undefined);
   });
 
-  it('infers common "viewedPage" event properties', () => {
+  it('infers common "viewedPage" event properties', async () => {
     document.title = 'Hello world';
 
     setConfig();
 
     H._ch('event', {type: 'viewedPage'});
 
-    const req = requests[0];
-    const props = JSON.parse(req.requestBody).properties;
+    await H.whenDone();
+
+    const body = H._fetchMock.lastOptions()?.body as unknown as string;
+    const props = JSON.parse(body).properties;
 
     expect(props.title).to.eql('Hello world');
     expect(props.url).to.match(/^http:.*context.html$/);
@@ -109,91 +99,101 @@ describe('Event API', () => {
     expect(props.referer).to.match(/^http:.*?id=.*$/);
   });
 
-  it('allows to override inferred properties', () => {
+  it('allows to override inferred properties', async () => {
     setConfig();
 
     H._ch('event', {type: 'viewedPage', properties: {title: 'Custom title'}});
 
-    const req = requests[0];
-    const props = JSON.parse(req.requestBody).properties;
+    await H.whenDone();
+
+    const body = H._fetchMock.lastOptions()?.body as unknown as string;
+    const props = JSON.parse(body).properties;
 
     expect(props.title).to.eql('Custom title');
     expect(props.path).to.eql('/context.html');
   });
 
-  it('does not infer properties on other event types', () => {
+  it('does not infer properties on other event types', async () => {
     document.title = 'Hello world';
 
     setConfig();
 
     H._ch('event', {type: 'something'});
 
-    const req = requests[0];
-    const props = JSON.parse(req.requestBody).properties;
+    await H.whenDone();
 
-    expect(props.title).to.equal(undefined);
-    expect(props.url).to.equal(undefined);
-    expect(props.path).to.equal(undefined);
-    expect(props.referer).to.equal(undefined);
+    const body = H._fetchMock.lastOptions()?.body as unknown as string;
+    const props = JSON.parse(body).properties;
+
+    expect(props).to.equal(undefined);
   });
 
-  it('gets the "context" from the cookie', () => {
+  it('gets the "context" from the cookie', async () => {
     setConfig();
 
     cookies.set(H.CH, {...cookies.getJSON(H.CH), context: 'FOO'});
 
     H._ch('event', {type: 'viewedPage'});
 
-    const req = requests[0];
-    const reqBody = JSON.parse(req.requestBody);
+    await H.whenDone();
 
-    expect(reqBody.context).to.equal('FOO');
+    const body = H._fetchMock.lastOptions()?.body as unknown as string;
+
+    expect(JSON.parse(body).context).to.equal('FOO');
   });
 
-  it('gets the "contextInfo" from the cookie', () => {
+  it('gets the "contextInfo" from the cookie', async () => {
     setConfig();
 
     cookies.set(H.CH, {...cookies.getJSON(H.CH), contextInfo: {foo: 'bar'}});
 
     H._ch('event', {type: 'viewedPage'});
 
-    const req = requests[0];
-    const reqBody = JSON.parse(req.requestBody);
+    await H.whenDone();
 
-    expect(reqBody.contextInfo).to.eql({foo: 'bar'});
+    const body = H._fetchMock.lastOptions()?.body as unknown as string;
+
+    expect(JSON.parse(body).contextInfo).to.eql({foo: 'bar'});
   });
 
   // --- Rejects
-  it('should throw and log error when event type is not defined', () => {
+  it('should log error when event type is not defined (no throws)', async () => {
     setConfig();
 
     expect(() => {
       H._ch('event', {} as any);
-    }).to.throw(Error);
+    }).not.to.throw();
+
+    await H.whenDone();
 
     expect(
-      spy.calledWith(
+      H.spy.calledWith(
         '[DEBUG] @contactlab/sdk-browser',
         'Missing required event type'
       )
     ).to.equal(true);
-
-    spy.restore();
   });
 
-  it('should log api call rejections', done => {
+  it('should log api call rejections', async () => {
+    H._fetchMock.post(`${H.API}/workspaces/${H.WSID}/events`, {
+      status: 500,
+      body: 'KO'
+    });
+
     setConfig();
 
     H._ch('event', {type: 'viewedPage'});
 
-    requests[0].respond(500, {}, 'KO');
+    await H.whenDone();
 
-    H.whenDone(() => {
-      expect(spy.calledWith('[DEBUG] @contactlab/sdk-browser', 'KO')).to.equal(
-        true
-      );
-
-      spy.restore();
-    }, done);
+    expect(
+      H.spy.calledWith(
+        '[DEBUG] @contactlab/sdk-browser',
+        'Request responded with status code 500'
+      )
+    ).to.equal(true);
   });
 });
+
+// --- Helpers
+const setConfig = (): void => H._ch('config', H.CONFIG);
