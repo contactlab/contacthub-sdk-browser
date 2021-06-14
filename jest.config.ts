0fe4ee5c5a68b03d2e222a06d56e6355102ec0ea
StefanoMagrassi <1291271+StefanoMagrassi@users.noreply.github.com>
2021-06-14 15:46:54 +0200

    setup jest

---
+++ jest.config.ts
@@ -0,0 +1,23 @@
+import type {Config} from '@jest/types';
+
+const config: Config.InitialOptions = {
+  preset: 'ts-jest',
+
+  automock: false,
+  bail: true,
+  collectCoverage: true,
+  coveragePathIgnorePatterns: ['<rootDir>/node_modules/'],
+  coverageReporters: ['text'],
+  globals: {
+    'ts-jest': {
+      diagnostics: true
+    }
+  },
+  moduleFileExtensions: ['js', 'json', 'node', 'ts'],
+  roots: ['<rootDir>/test/'],
+  testEnvironment: 'node',
+  testMatch: undefined,
+  testRegex: '(\\.|/)spec\\.ts$'
+};
+
+export default config;
